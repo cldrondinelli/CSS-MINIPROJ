1572150d3a532c69b8ddfb63b5ba0f3a205dc984
Rondinelli Lacampuingan Herrera <ronherrera011104@gmail.com>
2025-06-10 12:33:38 +0800

    Add files via upload

---
+++ CSS-MINI-PROJECT/HTMLJS/script.js
@@ -0,0 +1,12 @@
+function show_prompt()
+{
+var name=prompt("Please enter your name: ",
+
+"nameless");
+
+if (name!=null && name!="")
+{
+document.write("Welcome " + name + "! Please come in");
+
+}
+}
